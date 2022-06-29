26b00cf76944cc62fefb552dcb9dcd84c838ad1a
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-06-29 04:13:05 +0100

    made somc changes, added a loader, made it responsive

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 // function main() {
     const container = document.querySelector('.container');
+    const loader = document.querySelector('.loader');
 
     const displayPanel = function (e) {
       
@@ -7,11 +8,12 @@
       const accElement = e.target;
       accElement.classList.toggle("active");
       const panel = accElement.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display= "none";
-      } else {
-        panel.style.display = "block";
-      }
+      panel.classList.toggle("display")
+      // if (panel.style.display === "block") {
+      //   panel.style.display= "none";
+      // } else {
+      //   panel.style.display = "block";
+      // }
 
     }    
 
@@ -24,32 +26,45 @@
     
     
     const starWarPeople = async function () {
-      const res = await fetch('https://swapi.dev/api/people');
-      const { results } = await res.json();
 
-      results.forEach((element,i) => {
-        let { name, gender, height } = element;
+      try {
+        const res = await fetch('https://swapi.dev/api/people');
+        const { results } = await res.json();
+        loader.style.display = 'none'
+        console.log(loader);
 
-        container.innerHTML += `
-          <div class="child-container">
-            <button id="acc-${i}" class="accordion" data-id="${i}" onclick="displayPanel(event)">
-              ${i+1}. ${name}
-            </button>
-            <div class="panel">
-              <img class="img" src="images/img-${i}.jpeg" alt="">
-              <p>
-                Name: ${name}<br>
-                Heights: ${height}<br>
-                Gender: ${gender}
-              </p>
+        results.forEach((element,i) => {
+          let { name, gender, height } = element;
+          
+
+          container.innerHTML += `
+            <div class="child-container">
+              <button id="acc-${i}" class="accordion" data-id="${i}" onclick="displayPanel(event)">
+                ${i+1}. ${name}
+              </button>
+              <div class="panel display">
+                <img class="img" src="images/img-${i}.jpeg" alt="">
+                <p>
+                  Name: ${name}<br>
+                  Heights: ${height}<br>
+                  Gender: ${gender}
+                </p>
+              </div>
             </div>
-          </div>
-        
-        `;
-        
-        
-        
-      });
+          
+          `;
+          
+          
+          
+        });
+      } catch (error) {
+        // console.log(error);
+        // loader.style.display = 'block'
+          
+      }
+     
+
+      
 
     }
 
